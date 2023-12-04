904bb2f81aa0a917ca0ccd2743e44277e21d76ca
yuchul <drivegod5@naver.com>
2023-12-04 19:05:16 +0900

    numpy로 구현이되는 부분만 earlystop 코드로 수정

--- 2-2_refactoring/utils/earlystop.py
+++ 2-2_refactoring/utils/earlystop.py
@@ -8,33 +8,25 @@ class EarlyStopping:
         self.counter = 0
         self.best_score = None
         self.early_stop = False
-        self.val_loss_min = np.lnf
+        self.val_loss_min = np.inf
         self.delta = delta
         self.base_path = base_path
         self.save_path = save_path
     
     
-    def save_checkpoint(self, val_loss, model, optimizer, scheduler, scaler, epoch):
-        checkpoint_path = os.path.join(self.save_path, f"{self.base_path}_Best_{epoch+1}.pt")
-        checkpoint = {
-            'model_state_dict' : model.state_dict(),
-            'optimizer_state_dict' : optimizer.state_dict(),
-            'scheduler_state_dict' : scheduler.state_dict(),
-            'scaler_state_dict' : scaler.state_dict(),
-            'epoch' : epoch,
-            'val_loss' : val_loss
-        }
+    def save_checkpoint(self, val_loss, model, epoch):
+        checkpoint_path = os.path.join(self.save_path, f"{self.base_path}_Best_{epoch+1}.npz")
+        np.savez(checkpoint_path, **model.params)
         if self.verbose:
-            print(f"Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}). Saving model to {checkpoint} ...")
-        torch.save(checkpoint, checkpoint_path)
+            print(f"Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}). Saving model to {checkpoint_path} ...")
         self.val_loss_min = val_loss
         
-    def step(self, val_loss, model, optimizer, scheduler, scaler, epoch):
+    def step(self, val_loss, model, epoch):
         score = -val_loss
         
         if self.best_score is None:
             self.best_score = score
-            self.save_checkpoint(val_loss, model, optimizer, scheduler, scaler, epoch)
+            self.save_checkpoint(val_loss, model, epoch)
         elif score < self.best_score + self.delta:
             self.counter += 1
             print(f"EarlyStopping Count : {self.counter} out of {self.patience}")
@@ -43,7 +35,7 @@ class EarlyStopping:
         
         else:
             self.best_score = score
-            self.save_checkpoint(val_loss, model, optimizer, scheduler, scaler, epoch)
+            self.save_checkpoint(val_loss, model, epoch)
             self.counter = 0
     
             
